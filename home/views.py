8204a7b6ffc460cc4a95939720c88b1e83bc11c4
ThorRafnar <62342382+ThorRafnar@users.noreply.github.com>
2021-05-04 10:11:27 +0000

    products are available

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+
+import product.models
+from product import models
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    products = {
+        'product': product.models.Product.all()
+    }
+    return render(request, 'base.html', products)
 
 def home(request):
     return render(request, 'home/home.html')
