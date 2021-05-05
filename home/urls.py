9bca819914da267081bbee691f28d3e0a326ae84
Arnjee <arnist91@gmail.com>
2021-05-05 13:19:08 +0000

    contact us added

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('home/contact', views.contact,name='contact')
 ]
