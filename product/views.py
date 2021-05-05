ac5a1d3b03bce9f73fb3cfd3bae285c10c31d472
ThorRafnar <62342382+ThorRafnar@users.noreply.github.com>
2021-05-05 11:39:53 +0000

    product details connected

--- product/views.py
+++ product/views.py
@@ -7,15 +7,17 @@ from django.http import HttpResponse
 # Create your views here.
 def get_product_by_id(request, id):
     gallery = ProductGallery.objects.filter(product_id=id)
-    tags = Category.object.filter(product_id = id)
+    tags = Category.objects.filter(product = id)
+    prod = get_object_or_404(Product, pk=id)
     context = {
-        'product': get_object_or_404(Product, pk=id),
+        'product': prod,
         'images': [photo.image for photo in gallery],
         'nutrition': get_object_or_404(NutritionalInfo, pk=id),
-        'manufacturer': Manufacturer.objects.filter(product_id = id),
+        'manufacturer': get_object_or_404(Manufacturer, pk = prod.manufacturer_id_id),
         'categories': [tag.category for tag in tags]
     }
     print(context['product'].name)
     print(context['nutrition'].calories)
     print(context['categories'])
-    return render(request, 'product/single_product.html', context)
+    print(context['manufacturer'].name)
+    return render(request, 'products/product_details.html', context)
