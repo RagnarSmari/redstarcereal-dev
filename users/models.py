f474d440210d216f04dd41f01dcabd2de162a428
ThorRafnar <62342382+ThorRafnar@users.noreply.github.com>
2021-05-06 10:55:22 +0000

    users

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from pillow import
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default=default.jpg, upload_to ='media/profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='media/profile_pics')
+    def __str__(self):
+        return f'{user.username} profile'
+
