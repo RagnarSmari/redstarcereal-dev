02ec5599f9870eb352076485be5fc1bba5474c60
Ragnar Smári <raggism98@gmail.com>
2021-05-07 10:51:52 +0000

    Added js file and buttos now responsive

---
+++ static/js/index.js
@@ -0,0 +1,23 @@
+
+
+// Starting of filter buttons
+
+// Change the order of the product list in /home
+function changeDropDown(){
+    // Get req to some view
+    //
+}
+
+
+function getDropDownValue (){
+
+}
+
+// End of filter buttons
+
+
+
+
+
+
+
