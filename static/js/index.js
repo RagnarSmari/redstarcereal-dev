b26e85dc46a2f218a0e41baa405e54d27eb71828
Ragnar Smári <raggism98@gmail.com>
2021-05-09 15:00:19 +0000

    Closer and closer to this feature every day baby added some js to filter buttons, but requests need to be looked at

--- static/js/index.js
+++ static/js/index.js
@@ -11,19 +11,53 @@ $(".dropdown-menu li a").click(function () {
     $('#dropdownMenuButton1').html(selText);
 });
 
-$('#dropdownMenuButton1').on('click',function (){
-    console.log('im here')
-    let buttonValue = $(this).text();
-    console.log(buttonValue);
-})
 
-
-$('#dropdownMenuButton1').change(function (){
-    console.log('im here')
-    let buttonValue = $(this).text();
-    console.log(buttonValue);
+// Get request for filter by drop down button
+$(".dropdown-item").click(function (){
+    let sortParam = $(this).text();// This is the text that is on the buttons
+    sortParam = check_sort_param(sortParam);
+    console.log(sortParam);
+    $.ajax({
+        url: "http://localhost:8000/home/?order_by=" + sortParam,
+        type: 'GET',
+        success: function (res) {
+            console.log('success!');
+            alert(res);
+        }
+    })
 });
 
+
+// Get request for filter by drop down button
+$(".categories").click(function (){
+    $.ajax({
+        url: "http://localhost:8000/products/1",
+        type: 'GET',
+        success: function (res) {
+            console.log('success!!');
+            alert(res);
+        }
+    })
+})
+
+function check_sort_param(sortParam){
+    if (sortParam ===  '<p>Price &#129043</p>'){ // Price down
+        return '-price'
+    }
+    else if (sortParam === 'Price <p>&#129041</p>') { // Price up
+        return 'price'
+    }
+    else if (sortParam === 'Name <p>&#129043;</p>') { // Name down
+        return '-name'
+    }
+    else if (sortParam === 'Name <p>&#129041</p>') { // Name up
+        return 'name'
+    }
+    else {
+        return 'price'
+    }
+
+}
 
 
 
